ff2adfcc0e5c5a9ad4a5fb066c9410839f9a71d6
EY-X <e.yosefx128@gmail.com>
2019-07-28 18:24:56 -0400

    done ecercise

---
+++ photogur/views.py
@@ -0,0 +1,15 @@
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render
+from photogur.models import *
+
+def pictures_view(request):
+
+
+
+    context = { 
+        'pictures': Picture.objects.all()
+    }
+
+
+    response = render(request, 'pictures.html', context)
+    return HttpResponse(response)
