6059b3a7a042103dbd8eef633e6f529faad418d1
EY-X <e.yosefx128@gmail.com>
2019-07-31 12:19:25 -0400

    comments added and saved to database AND redirected to picture id path

--- photogur/views.py
+++ photogur/views.py
@@ -27,5 +27,15 @@ def picture_search(request):
     response = render(request, 'search.html', context)
     return HttpResponse(response)
 
+def create_comment(request):
+    picture = request.POST['picture']
+    picture_id = Picture.objects.get(pk=picture)
+    name = request.POST['name_comment_name']
+    message = request.POST['message_comment_name']
+    new_comment = Comment(name=name, message=message, picture=picture_id)
+    new_comment.save()
+    context = {'picture': picture_id}
+    return render(request,'picture.html', context)
+    # return HttpResponseRedirect(f'/pictures/{picture}')
 
 
