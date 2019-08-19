fc57360fcf61cb80ac51a17ab604f1a781df6307
EY-X <e.yosefx128@gmail.com>
2019-08-19 01:46:01 -0400

    Added sign up

--- photogur/views.py
+++ photogur/views.py
@@ -32,6 +32,21 @@ def logout_view(request):
     logout(request)
     return HttpResponseRedirect('/pictures')   
 
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return HttpResponseRedirect('pictures')
+    else:
+        form = UserCreationForm()
+    html_response =  render(request, 'signup.html', {'form': form})
+    return HttpResponse(html_response)
+
 
 @login_required
 def function_name(request):
